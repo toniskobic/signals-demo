d2265cfab80d3ef381fccb42bb25608503e64f58
tskobic <toni.skobic@gmail.com>
2023-04-12 13:55:52 +0200

    Countdown component refactored

--- src/app/components/countdown/countdown.component.ts
+++ src/app/components/countdown/countdown.component.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-countdown',
@@ -9,18 +10,33 @@ import { Component, signal } from '@angular/core';
 export class CountdownComponent {
 
   timer = signal({ minutes: 5, seconds: 0 });
+  endDate: Date | null = null;
   intervalTimer: NodeJS.Timer | null = null;
 
   play() {
+    this.endDate = moment(new Date()).add(this.timer().minutes, 'm').add(this.timer().seconds, 's').toDate();
+    this.calculateTimeLeft();
     this.intervalTimer = setInterval(() => {
-      if (this.timer().seconds) {
-        this.timer.mutate((timer) => timer.seconds -= 1);
-      } else if (this.timer().minutes) {
-        this.timer.mutate((timer) => { timer.minutes = timer.minutes - 1, timer.seconds = 59 });
+      this.calculateTimeLeft();
+    }, 1000);
+  }
+
+  calculateTimeLeft() {
+    if (this.endDate) {
+      const diff = this.endDate.valueOf() - new Date().valueOf();
+      if (diff > 0) {
+        let minutes = Math.floor(diff / 60000);
+        let seconds = Math.trunc(((diff % 60000) / 1000));
+        if (seconds == 60) {
+          minutes += 1;
+          seconds = 0;
+        }
+        this.timer.mutate((timer) => { timer.minutes = minutes, timer.seconds = seconds });
       } else {
         this.tryClearInterval();
+        this.endDate = null;
       }
-    }, 1000);
+    }
   }
 
   tryClearInterval() {
