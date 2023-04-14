7b6f995b61cd159a271463003d359e7b7d84dcb3
tskobic <toni.skobic@gmail.com>
2023-04-14 10:42:24 +0200

    Minor changes

--- src/app/components/grandchild/grandchild.component.ts
+++ src/app/components/grandchild/grandchild.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input, WritableSignal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-grandchild',
   templateUrl: './grandchild.component.html',
   styleUrls: ['./grandchild.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [FormsModule]
 })
 export class GrandchildComponent {
 
   @Input() data!: WritableSignal<string>;
 
-  setValue(input: string){
+  setValue(input: string) {
     console.log('setValue called');
     this.data.set(input);
   }
