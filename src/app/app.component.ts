15cc4884e50ea94049135e67adb0ba730eeb7a21
tskobic <toni.skobic@gmail.com>
2023-05-09 13:59:44 +0200

    Added counter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CountdownComponent } from './components/countdown/countdown.component';
 import { UsersComponent } from "./components/users/users.component";
 import { FilteredUsersComponent } from "./components/filtered-users/filtered-users.component";
 import { ParentComponent } from "./components/parent/parent.component";
+import { CounterComponent } from "./components/counter/counter.component";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [CountdownComponent, UsersComponent, FilteredUsersComponent, ParentComponent]
+    imports: [CountdownComponent, UsersComponent, FilteredUsersComponent, ParentComponent, CounterComponent]
 })
 export class AppComponent {
 }
---
+++ src/app/components/counter/counter.component.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss'],
+  standalone: true,
+})
+export class CounterComponent {
+
+  counter = signal(0);
+
+  increment() {
+    this.counter.update(counter => counter + 1);
+  }
+}
